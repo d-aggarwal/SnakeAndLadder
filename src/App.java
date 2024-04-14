b2b115a0ba33181f02a45641d5552fed7d53a2d7
d-aggarwal <drishtiaggarwal6284@gmail.com>
2024-04-14 15:10:08 +0530

    coordinates of jumper initiated

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import board.Board;
+import jumper.Jumper;
+import coordinates.Coordinates;
+
+public class App {
+    public static void main(String[] args) {
+
+        HashMap<String,Jumper> map = new HashMap<>();
+        map.put("6",new Jumper(new Coordinates(9,5),new Coordinates(6,6)));
+        map.put("97",new Jumper(new Coordinates(0,1),new Coordinates(9,9)));
+        map.put("21",new Jumper(new Coordinates(7,0),new Coordinates(0,9)));
+        map.put("64",new Jumper(new Coordinates(3,3),new Coordinates(7,4)));
+//        /    Player p1 =  new Player();
+//    p1.setPlayerDetailsWithUserInput();
+//    Player p2 =  new Player();
+//    p2.setPlayerDetailsWithUserInput();
+//
+//    System.out.println(p1.getPlayerName());
+//    System.out.println(p2.getPlayerName());
+
+Board b = new Board(10);
+b.printBoard();
+
+
+
+
+
+    }
+
+
+}
